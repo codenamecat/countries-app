e6c7b2cb55d790af54f9ef041fc28eafeb447600
codenamecat <113186187+codenamecat@users.noreply.github.com>
2023-06-07 14:39:57 +0300

    links to border countries added

--- src/components/CountryInfoLong.jsx
+++ src/components/CountryInfoLong.jsx
@@ -1,52 +1,76 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import countryData from '../data.json'
-import backArrow from '../images/arrow-back-outline.svg'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import countryData from '../data.json';
+import backArrow from '../images/arrow-back-outline.svg';
 
 export default function CountryInfoLong() {
     const location = useLocation();
     const countryCode = location.state.countryCode;
 
-    const country = countryData.find(item => item.alpha3Code === countryCode)
+    const country = countryData.find((item) => item.alpha3Code === countryCode);
     // console.log(country)
-    const topLevelDomains = country.topLevelDomain.map(domain => {
-        return <span key={domain}>{domain}</span>
-    })
+    const topLevelDomains = country.topLevelDomain.map((domain) => {
+        return <span key={domain}>{domain}</span>;
+    });
 
     const currencies = country.currencies?.map((currency, index) => {
-        return <span key={currency.code}>{currency.name}{index < country.currencies.length - 1 ? ", " : ""}</span>
-    })
+        return (
+            <span key={currency.code}>
+                {currency.name}
+                {index < country.currencies.length - 1 ? ', ' : ''}
+            </span>
+        );
+    });
 
     const languages = country.languages?.map((language, index) => {
-        return <span key={language.name}>{language.name}{index < country.languages.length - 1 ? ", " : ""}</span>
-    })
+        return (
+            <span key={language.name}>
+                {language.name}
+                {index < country.languages.length - 1 ? ', ' : ''}
+            </span>
+        );
+    });
 
-    const borderCountries = country.borders?.map(item => {
-        return <button key={item}>{item}</button>
-    })
+    const borderCountries = country.borders?.map((borderCountryCode) => {
+        const borderCountry = countryData.find(
+            (thisCountry) => thisCountry.alpha3Code === borderCountryCode
+        );
+
+        return (
+            <Link to={`/${borderCountry.name}`} state={{countryCode: borderCountryCode}} key={borderCountryCode}>
+                <button>{borderCountry.name}</button>
+            </Link>
+        );
+    });
+
+    const formatPopulation = new Intl.NumberFormat().format(country.population);
 
     return (
         <div>
-            <Link to="/">
+            <Link to='/'>
                 <button>
-                    <img src={backArrow} className="back-arrow" />
+                    <img src={backArrow} className='back-arrow' />
                     Back
                 </button>
             </Link>
-            <img src={country.flags.svg} className="flag-img" />
+            <img src={country.flags.svg} className='flag-img' />
             <h2>{country.name}</h2>
             <ul>
-                <li>Native name: {country.nativeName || "n/a"}</li>
-                <li>Population: {country.population || "n/a"}</li>
-                <li>Region: {country.region || "n/a"}</li>
-                <li>Sub Region: {country.subregion || "n/a"}</li>
-                <li>Capital: {country.capital || "n/a"}</li>
-                <li>Top Level Domain: {topLevelDomains || "n/a"}</li>
-                <li>Currencies: {currencies || "n/a"}</li>
-                <li>Languages: {languages || "n/a"}</li>
+                <li>Native name: {country.nativeName || 'n/a'}</li>
+                <li>Population: {formatPopulation || 'n/a'}</li>
+                <li>Region: {country.region || 'n/a'}</li>
+                <li>Sub Region: {country.subregion || 'n/a'}</li>
+                <li>Capital: {country.capital || 'n/a'}</li>
+                <li>Top Level Domain: {topLevelDomains || 'n/a'}</li>
+                <li>Currencies: {currencies || 'n/a'}</li>
+                <li>Languages: {languages || 'n/a'}</li>
             </ul>
             <h3>Border Countries: </h3>
-            {borderCountries ? <div>{borderCountries}</div> : <p>No border countries!</p>}
+            {borderCountries ? (
+                <div>{borderCountries}</div>
+            ) : (
+                <p>No border countries!</p>
+            )}
         </div>
-    )
+    );
 }
--- src/components/CountryInfoShort.jsx
+++ src/components/CountryInfoShort.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function CountryInfoShort(props) {
+
+    const formatPopulation = new Intl.NumberFormat().format(props.population);
+
     return (
         <div>
             <Link to={props.name} state={{countryCode: props.code}}>
                 <img src={props.flagImg} alt={`The flag of ${props.name}`} className="flag-img" />
                 <h2>{props.name}</h2>
                 <ul>
-                    <li>Population: {props.population}</li>
+                    <li>Population: {formatPopulation}</li>
                     <li>Region: {props.region}</li>
                     <li>Capital: {props.capital}</li>
                 </ul>
