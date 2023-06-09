0dc56687611b1c464019681970175454621deb8b
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-06-09 10:47:18 +0300

    main page styling

--- src/components/CountryInfoShort.jsx
+++ src/components/CountryInfoShort.jsx
@@ -6,15 +6,17 @@ export default function CountryInfoShort(props) {
     const formatPopulation = new Intl.NumberFormat().format(props.population);
 
     return (
-        <div>
-            <Link to={props.name} state={{countryCode: props.code}}>
+        <div className="country-card">
+            <Link to={props.name} state={{ countryCode: props.code }}>
                 <img src={props.flagImg} alt={`The flag of ${props.name}`} className="flag-img" />
-                <h2>{props.name}</h2>
-                <ul>
-                    <li>Population: {formatPopulation}</li>
-                    <li>Region: {props.region}</li>
-                    <li>Capital: {props.capital}</li>
-                </ul>
+                <div className="country-info">
+                    <h2 className="country-name">{props.name}</h2>
+                    <ul>
+                        <li><span className="data-title">Population:</span> {formatPopulation || "n/a"}</li>
+                        <li><span className="data-title">Region:</span> {props.region || "n/a"}</li>
+                        <li><span className="data-title">Capital:</span> {props.capital || "n/a"}</li>
+                    </ul>
+                </div>
             </Link>
         </div>
     )
